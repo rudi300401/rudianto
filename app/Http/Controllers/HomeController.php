335c3e64d62e25b7ca7de3612d5917d27e45402c
rudi300401 <rudy76321@gmail.com>
2023-08-27 18:33:52 +0700

    Eloquent

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
+use App\Models\Member;
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +26,11 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // $member = member::with('user')->get();
+        // $books = Book::with('Publisher')->get();
+        $Books = Book::with ('Catalog')->get();
+
+        return $Books;
         return view('home');
     }
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    public function Catalog ()
+    // public function Publisher ()
+    // public function Author ()
+    {
+      
+        return $this->belongsTo('App\Models\Catalog', 'Catalog_id');
+        // return $this->belongsTo('App\Models\Author', 'author_id');
+        // return $this->belongsTo('App\Models\Publisher', 'publisher_id');
+    }
 }
