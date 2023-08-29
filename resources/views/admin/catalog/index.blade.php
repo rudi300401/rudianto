46b68a06afab27575cc613a2f4321e4030a94531
rudi300401 <rudy76321@gmail.com>
2023-08-29 20:51:32 +0700

    Membuat data untuk index catalog

--- resources/views/admin/catalog/index.blade.php
+++ resources/views/admin/catalog/index.blade.php
@@ -1,6 +1,38 @@
-@extends('layouts.admin')
-@section('header', 'catalog')
+    @extends('layouts.admin')
+    @section('header', 'catalog')
 
-@section('content')
-ini halaman catalog
-@endsection
+    @section('content')
+
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                <h3 class="card-title">Catalog</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th style="width: 10px">#</th>
+                        <th>Name</th>
+                        <th>Total buku</th>
+                        <th>created_at</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($catalogs as $key => $catalog)
+                    <tr>
+                        <td class="text-center">{{$key+1}}</td>
+                        <td> {{$catalog->name}} </td>
+                        <td class="text-center"> {{count ($catalog->books) }} </td>
+                        <td class="text-center"> {{$catalog->created_at}} </td>
+                        @endforeach
+                    </tbody>
+                </table>
+                </div>
+    @endsection
